25ca3d962e9be4d379d5639fac926925416edef0
Yurun <admin@yurunsoft.com>
2021-08-29 08:30:04 +0800

    Fix workerman

--- frameworks/PHP/imi/Listener/AppInit.php
+++ frameworks/PHP/imi/Listener/AppInit.php
@@ -19,7 +19,7 @@ class AppInit implements IEventListener
      */
     public function handle(EventParam $e): void
     {
-        if(getenv('WITH_REDIS') ?? false)
+        if(getenv('WITH_REDIS') ?: false)
         {
             $redis = RedisManager::getInstance();
             $page = 1;
